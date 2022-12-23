261db8cb3aa4e5cb0edcc689b81e0be9d580d33e
shiftybody <shiftybody@gmail.com>
2022-12-23 00:53:04 -0600

    working with tailwindcss

--- includes/DVerificaciones.php
+++ includes/DVerificaciones.php
@@ -9,7 +9,7 @@
     $Con = Conectar();
     $Result = Ejecutar($Con, $SQL) or die ("Error al eliminar datos".mysqli_error($Con));
     $FilasAfectadas = mysqli_affected_rows($Con);
-
+    
     print("<br> Numero de Filas Eliminadas:" . $FilasAfectadas);
 
     Cerrar($Con);
